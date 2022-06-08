8ab8132e7479bba3b25b9a318f02205afa9594ea
kranthi-choundur <104243652+kranthi-choundur@users.noreply.github.com>
2022-06-08 23:18:24 +0530

    Add files via upload

---
+++ simple linear regression.r
@@ -0,0 +1,72 @@
+
+#import the dataset
+
+dataset =read.csv("Admission_Predict.csv")
+View(dataset)
+
+#slicing the data
+
+gre=dataset[ ,2]
+gre
+
+coa=dataset[ ,9]
+coa
+
+#dataset for slr
+
+data=data.frame(gre,coa)
+View(data)
+
+#import data splitting library
+
+library(caTools)
+
+#generate random numbers
+
+set.seed(123)
+
+#data splitting
+
+Split1=sample.split(data$coa,SplitRatio = 0.8)
+View(Split1)
+
+#training set
+
+traning_set=subset(data,Split== TRUE)
+
+#testing
+
+testing_set=subset(data,Split1== FALSE)
+View(testing_set)
+
+#apply regression(training the model)
+
+regressor=lm(formula = coa ~ gre,data=traning_set)#lm=linear model
+
+summary(regressor)
+
+#prediction testing phase
+
+y_predict=predict(regressor,newdata = testing_set)
+View(y_predict)
+y_predict
+
+#visualization
+
+plot(testing_set$gre,testing_set$coa,type="p",
+     col='blue',xlab='gre',ylab='coa')    #plot=scatter plot
+
+
+lines(testing_set$gre,y_predict,type='o',col='red')
+
+
+#visualization using ggplot
+
+library(ggplot2)
+
+ggplot(data,geom_point(aes(x=testing_set$gre,y=testing_set$coa,
+                      color='blue'))
+         +geom_line(aes(x=testing_set$gre,y=y_predict,
+                      color='red'))
+                  +ggtitle("GRE SCORE VERSUS CHANCE OF ADMIT")
+                  +xlab("GRE SCORE")+ylab("CHANCE OF ADMIT"))
